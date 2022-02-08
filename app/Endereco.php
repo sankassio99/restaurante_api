1e8720fe66faf450ab1b3d7b4d1fee2bef20e218
sankassio99 <kciosantana@gmail.com>
2022-02-08 20:26:17 -0300

    fillables fields endereco

--- app/Endereco.php
+++ app/Endereco.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Endereco extends Model
 {
     protected $table = 'endereco' ;
+    protected $fillable = ["rua", "complemento", "bairro", "cidade", "estado", "cep", "long", "lat"];
+    public $timestamps = false;
 }
